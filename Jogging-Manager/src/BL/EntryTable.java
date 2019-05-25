524f59ea9ff2ee28bb81e7241572908b85383e7a
Chris Schennach <senchb16@new.edvhtl.at>
2019-05-25 15:52:38 +0200

    created Project and Classes

---
+++ Jogging-Manager/src/BL/EntryTable.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Christoph
+ */
+public class EntryTable extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        
+    }
+
+    @Override
+    public int getColumnCount() {
+        
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        
+    }
+    
+}
---
+++ Jogging-Manager/src/DataBase/DataBaseConnection.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBase;
+
+import GUI.MainGUI;
+
+/**
+ *
+ * @author Christoph
+ */
+public class DataBaseConnection {
+     
+    public DataBaseConnection(String user, String password) {
+        
+    }
+    
+        
+        
+    
+    
+}
